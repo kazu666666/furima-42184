9f4688f8a7aa5e86de553d5c378cd5f2f728971d
kazu666666 <kmf2fl@gmail.com>
2025-03-13 14:03:10 +0900

    バリデーションまで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,21 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  def new
+    @items = Items.new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,21 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+  
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+
+  validates :name,           presence: true, length: { maximum: 40 }
+  validates :description,    presence: true, length: { maximum: 1000 }
+  validates :category_id,    presence: true, numericality: { other_than: 0 }
+  validates :status_id,      presence: true, numericality: { other_than: 0 }
+  validates :shipping_id,    presence: true, numericality: { other_than: 0 }
+  validates :prefecture_id,  presence: true, numericality: { other_than: 0 }
+  validates :shipping_day_id,presence: true, numericality: { other_than: 0 }
+  validates :price,          presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10_000_000}
+
+end
