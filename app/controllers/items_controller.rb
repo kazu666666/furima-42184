b4bc91dd57df6f44d6a2525ecf4d3415468ca5d5
kazu666666 <kmf2fl@gmail.com>
2025-03-16 13:51:55 +0900

    エラーの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -23,8 +23,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  #def update
-  #end
+  def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+      redirect_to item_path(@item)
+     else
+      render :edit, status: :unprocessable_entity
+     end 
+  end
 
   def edit
     @item = Item.find(params[:id])
@@ -39,5 +45,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:name,:description,:category_id,:status_id,:shipping_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
   
 end
